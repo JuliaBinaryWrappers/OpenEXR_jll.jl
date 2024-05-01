ff0d27339b84a265808bf52036cadf6a7f0652f1
jlbuild <juliabuildbot@gmail.com>
2024-05-01 08:53:40 +0000

    OpenEXR_jll build 3.2.4+0

--- src/wrappers/x86_64-linux-musl-cxx11.jl
+++ src/wrappers/x86_64-linux-musl-cxx11.jl
@@ -4,40 +4,40 @@ export libIex, libIlmThread, libOpenEXR, libOpenEXRCore, libOpenEXRUtil
 using Imath_jll
 using Zlib_jll
 JLLWrappers.@generate_wrapper_header("OpenEXR")
-JLLWrappers.@declare_library_product(libIex, "libIex-3_1.so.29")
-JLLWrappers.@declare_library_product(libIlmThread, "libIlmThread-3_1.so.29")
-JLLWrappers.@declare_library_product(libOpenEXR, "libOpenEXR-3_1.so.29")
-JLLWrappers.@declare_library_product(libOpenEXRCore, "libOpenEXRCore-3_1.so.29")
-JLLWrappers.@declare_library_product(libOpenEXRUtil, "libOpenEXRUtil-3_1.so.29")
+JLLWrappers.@declare_library_product(libIex, "libIex-3_2.so.31")
+JLLWrappers.@declare_library_product(libIlmThread, "libIlmThread-3_2.so.31")
+JLLWrappers.@declare_library_product(libOpenEXR, "libOpenEXR-3_2.so.31")
+JLLWrappers.@declare_library_product(libOpenEXRCore, "libOpenEXRCore-3_2.so.31")
+JLLWrappers.@declare_library_product(libOpenEXRUtil, "libOpenEXRUtil-3_2.so.31")
 function __init__()
     JLLWrappers.@generate_init_header(Imath_jll, Zlib_jll)
     JLLWrappers.@init_library_product(
         libIex,
-        "lib/libIex-3_1.so",
+        "lib/libIex-3_2.so",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libIlmThread,
-        "lib/libIlmThread-3_1.so",
+        "lib/libIlmThread-3_2.so",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libOpenEXR,
-        "lib/libOpenEXR-3_1.so",
+        "lib/libOpenEXR-3_2.so",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libOpenEXRCore,
-        "lib/libOpenEXRCore-3_1.so",
+        "lib/libOpenEXRCore-3_2.so",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libOpenEXRUtil,
-        "lib/libOpenEXRUtil-3_1.so",
+        "lib/libOpenEXRUtil-3_2.so",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
--- src/wrappers/x86_64-w64-mingw32-cxx11.jl
+++ src/wrappers/x86_64-w64-mingw32-cxx11.jl
@@ -4,40 +4,40 @@ export libIex, libIlmThread, libOpenEXR, libOpenEXRCore, libOpenEXRUtil
 using Imath_jll
 using Zlib_jll
 JLLWrappers.@generate_wrapper_header("OpenEXR")
-JLLWrappers.@declare_library_product(libIex, "libIex-3_1.dll")
-JLLWrappers.@declare_library_product(libIlmThread, "libIlmThread-3_1.dll")
-JLLWrappers.@declare_library_product(libOpenEXR, "libOpenEXR-3_1.dll")
-JLLWrappers.@declare_library_product(libOpenEXRCore, "libOpenEXRCore-3_1.dll")
-JLLWrappers.@declare_library_product(libOpenEXRUtil, "libOpenEXRUtil-3_1.dll")
+JLLWrappers.@declare_library_product(libIex, "libIex-3_2.dll")
+JLLWrappers.@declare_library_product(libIlmThread, "libIlmThread-3_2.dll")
+JLLWrappers.@declare_library_product(libOpenEXR, "libOpenEXR-3_2.dll")
+JLLWrappers.@declare_library_product(libOpenEXRCore, "libOpenEXRCore-3_2.dll")
+JLLWrappers.@declare_library_product(libOpenEXRUtil, "libOpenEXRUtil-3_2.dll")
 function __init__()
     JLLWrappers.@generate_init_header(Imath_jll, Zlib_jll)
     JLLWrappers.@init_library_product(
         libIex,
-        "bin\\libIex-3_1.dll",
+        "bin\\libIex-3_2.dll",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libIlmThread,
-        "bin\\libIlmThread-3_1.dll",
+        "bin\\libIlmThread-3_2.dll",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libOpenEXR,
-        "bin\\libOpenEXR-3_1.dll",
+        "bin\\libOpenEXR-3_2.dll",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libOpenEXRCore,
-        "bin\\libOpenEXRCore-3_1.dll",
+        "bin\\libOpenEXRCore-3_2.dll",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libOpenEXRUtil,
-        "bin\\libOpenEXRUtil-3_1.dll",
+        "bin\\libOpenEXRUtil-3_2.dll",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
